f9414a2bef4c1a55b97a04c96e4f69c71da4a8f0
homoeconomicustapiales <homoeconomicus.unlam@gmail.com>
2026-03-31 09:04:20 -0300

    Update isac.ts

--- my-isac-dashboard/src/types/isac.ts
+++ my-isac-dashboard/src/types/isac.ts
@@ -5,6 +5,8 @@ export interface IsacNivelGeneral {
   original: number;
   desestacionalizada: number;
   tendencia_ciclo: number;
+  // Añade esta línea:
+  [key: string]: string | number | undefined;
 }
 
 export interface IsacInsumo {
@@ -22,19 +24,24 @@ export interface IsacInsumo {
   placas_yeso: number;
   yeso: number;
   resto: number;
+  // También añade esta línea por si usas variaciones en insumos:
+  [key: string]: string | number | undefined;
 }
 
 export interface IsacPuestosTrabajo {
   fecha: string;
   puestos_trabajo: number;
+  [key: string]: string | number | undefined;
 }
 
 export interface IsacPermisos {
   fecha: string;
   superficie_m2: number;
   permisos_cantidad: number;
+  [key: string]: string | number | undefined;
 }
 
 export type InsumoKey = keyof Omit<IsacInsumo, 'fecha'>;
 export type SerieNivelKey = keyof Omit<IsacNivelGeneral, 'fecha'>;
 export type TipoSerie = 'original' | 'desest' | 'tendencia';
+
